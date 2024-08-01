6c33c2089051328b3c16a66311fbe7dd0df2df72
petersudai <psudai@gmail.com>
2024-08-01 13:56:04 +0000

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Filtered logger
+"""
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Obfuscates the specified fields in a log message
+    """
+    pattern = '|'.join(f'{field}=[^{separator}]*' for field in fields)
+    return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}", message)
