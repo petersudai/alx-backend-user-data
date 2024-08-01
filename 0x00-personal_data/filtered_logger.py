73feadd23b550eea920722c25eb921a9e55f4062
petersudai <psudai@gmail.com>
2024-08-01 14:17:55 +0000

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 Filtered logger
 """
 
+import logging
 import re
 from typing import List
 
@@ -22,3 +23,29 @@ def filter_datum(
         pattern,
         lambda m: f"{m.group().split('=')[0]}={redaction}",
         message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter Class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initialize formatter with specified fields to redact
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formaat the log record, filtering sensitive data
+        """
+        original_message = super(RedactingFormatter, self).format(record)
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            original_message,
+            self.SEPARATOR)
