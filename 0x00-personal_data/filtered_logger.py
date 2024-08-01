9268716d763482b44532d2fd233f9f63f1d69527
petersudai <psudai@gmail.com>
2024-08-01 14:09:15 +0000

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,11 +7,17 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     """
     Obfuscates the specified fields in a log message
     """
-    pattern = r'({})=[^{}]*'.format('|'.join(re.escape(field) for field in fields), re.escape(separator))
+    pattern = r'({})=[^{}]*'.format('|'.join(re.escape(field)
+                                             for field in fields),
+                                    re.escape(separator))
     return re.sub(
         pattern,
         lambda m: f"{m.group().split('=')[0]}={redaction}",
