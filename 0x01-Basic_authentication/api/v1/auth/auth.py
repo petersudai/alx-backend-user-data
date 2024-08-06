062f6f54719ee545738c7a0818f1d25074d8e8fe
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-06 23:20:39 +0300

    Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,10 +5,14 @@ Auth module for the API
 from typing import List, TypeVar
 from flask import request
 
+
 User = TypeVar('User')
 
 
 class Auth:
+    """
+    authenticating users
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Method that returns False. This method will be used later.
