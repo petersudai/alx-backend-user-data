3d65233405455a26fbf4cf1f0a193d830aa3ddf3
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-06 23:15:04 +0300

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Auth module for the API
+"""
+from typing import List, TypeVar
+from flask import request
+
+User = TypeVar('User')
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Method that returns False. This method will be used later.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Method that returns None. This method will be used later.
+        """
+        return None
+
+    def current_user(self, request=None) -> User:
+        """
+        Method that returns None. This method will be used later.
+        """
+        return None
