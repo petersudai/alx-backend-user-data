a4e218626087d3ed4b76a39fdb5a2b9ee28185e1
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-09 21:26:23 +0300

    Basic - Overload current_user - and BOOM!

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -113,3 +113,16 @@ class BasicAuth(Auth):
             return None
 
         return self.user_object_from_credentials(user_email, user_pwd)
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Returns the user email and password from the Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None or not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' in decoded_base64_authorization_header:
+            email, password = decoded_base64_authorization_header.split(':', 1)
+            return email, password
+
+        return None, None
