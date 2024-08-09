eb680ed945cda495e35fd6eee6d35bc4f0ed135e
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-09 15:06:08 +0300

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,17 +8,34 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
-
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
+auth_type = getenv("AUTH_TYPE")
 
-AUTH_TYPE = getenv("AUTH_TYPE")
-if AUTH_TYPE == 'auth':
+if auth_type == "basic_auth":
+    auth = BasicAuth()
+else:
     auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """
+    Method to handle filtering before each request
+    """
+    if auth:
+        excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+        if not auth.require_auth(request.path, excluded_paths):
+            return
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(404)
@@ -40,29 +57,6 @@ def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
-
-
-@app.before_request
-def before_request():
-    """
-    Method to handle request filtering before processing
-    """
-    if auth is None:
-        return
-
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/']
-
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)
 
 
 if __name__ == "__main__":
