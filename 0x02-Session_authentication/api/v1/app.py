6f726b63a97b606238b55192bf478c2083ca0210
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-11 01:47:23 +0300

    Sessions in database

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,6 +11,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -20,7 +21,9 @@ auth = None
 auth_type = getenv("AUTH_TYPE")
 
 
-if auth_type == "session_exp_auth":
+if auth_type == "session_db_auth":
+    auth = SessionDBAuth()
+elif auth_type == "session_exp_auth":
     auth = SessionExpAuth()
 elif auth_type == "session_auth":
     auth = SessionAuth()
