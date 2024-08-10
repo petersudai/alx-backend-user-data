ed1b15e69c232d943e2f97d1a98738aa0e9c5e1e
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-11 01:07:31 +0300

    Logout

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -50,3 +50,21 @@ class SessionAuth(Auth):
         user = User.get(user_id)
 
         return user
+
+    def destroy_session(self, request=None) -> bool:
+        """
+        Deletes user session (logout)
+        """
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+
+        del self.user_id_by_session_id[session_id]
+        return True
