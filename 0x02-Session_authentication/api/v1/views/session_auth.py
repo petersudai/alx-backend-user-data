c312a503f5cc170f12fed1e5c584e526cacc0b05
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-11 01:16:45 +0300

    Logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -54,7 +54,10 @@ def login():
 
     return response
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'],
+                 strict_slashes=False)
 def logout():
     """
     Handle logout for session authentication
