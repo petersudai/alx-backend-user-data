11302dea1ae13dc647fa39b105ce709484d9913b
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-11 01:36:43 +0300

    Update the users.py file

--- 0x02-Session_authentication/models/user.py
+++ 0x02-Session_authentication/models/user.py
@@ -28,7 +28,7 @@ class User(Base):
     def password(self, pwd: str):
         """ Setter of a new password: encrypt in SHA256
         """
-        if pwd is None or type(pwd) is not str:
+        if pwd is None or not isinstance(pwd, str):
             self._password = None
         else:
             self._password = hashlib.sha256(pwd.encode()).hexdigest().lower()
@@ -36,24 +36,23 @@ class User(Base):
     def is_valid_password(self, pwd: str) -> bool:
         """ Validate a password
         """
-        if pwd is None or type(pwd) is not str:
+        if pwd is None or not isinstance(pwd, str):
             return False
         if self.password is None:
             return False
-        pwd_e = pwd.encode()
-        return hashlib.sha256(pwd_e).hexdigest().lower() == self.password
+        return hashlib.sha256(
+            pwd.encode()).hexdigest().lower() == self.password
 
     def display_name(self) -> str:
         """ Display User name based on email/first_name/last_name
         """
-        if self.email is None and self.first_name is None \
-                and self.last_name is None:
-            return ""
-        if self.first_name is None and self.last_name is None:
-            return "{}".format(self.email)
-        if self.last_name is None:
-            return "{}".format(self.first_name)
-        if self.first_name is None:
-            return "{}".format(self.last_name)
-        else:
-            return "{} {}".format(self.first_name, self.last_name)
+        if self.email:
+            if self.first_name and self.last_name:
+                return "{} {}".format(self.first_name, self.last_name)
+            elif self.first_name:
+                return "{}".format(self.first_name)
+            elif self.last_name:
+                return "{}".format(self.last_name)
+            else:
+                return self.email
+        return ""
