a5b3a8caaab3017181aefea3fab6799ea2f4f73e
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-13 22:38:51 +0300

    create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,7 +34,7 @@ class DB:
             return
         new_user = User(email=email, hashed_password=hashed_password)
 
-        self._session.add(new_user)
-        self._session.commit()
+        session.add(new_user)
+        session.commit()
 
         return new_user
