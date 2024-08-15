b2784dfb7e56a1568233ec6f61c6b6760a57ab8e
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-15 20:59:36 +0300

    Generate UUIDs

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,6 +17,7 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
+
 def _generate_uuid() -> str:
     """
     Generates a new UUID and returns its string representation
