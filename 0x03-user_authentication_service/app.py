a694e36714196da04719463acc4efcd57ad4066a
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-15 23:01:32 +0300

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-Basic Flask app
+Flask app
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
