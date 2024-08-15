ff058465d86cf8a7786b9442017623ff91f1314c
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-15 16:25:40 +0300

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,5 +17,23 @@ def welcome():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=["POST"])
+def users():
+    """
+    POST /users route to register a new user
+    """
+    email = request.form.get("email")
+    password = request.form.gete("password")
+
+    if not email or not password:
+        return jsonify({"message": "email and password are required"}), 400
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
