e34e50cc72c4f8c0a73bcddf59d5c7ae608cfbb6
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-15 20:41:28 +0300

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,8 +45,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(
-                password.encode('utf-8'),
-                user.hashed_password)
+            if user and bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+            return False
         except NoResultFound:
             return False
