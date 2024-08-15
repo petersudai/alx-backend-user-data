85404cc291ad0dbf958c7a7cc554985f6310723f
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-15 23:00:07 +0300

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -42,5 +42,28 @@ def users():
         return jsonify({"message": "unexpected error occurred"}), 500
 
 
+@app.route("/sessions", methods=["POST"])
+def login():
+    """
+    POST /sessions route to log in a user and create a session
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        abort(401)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    if not session_id:
+        abort(401)
+
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie("session_id", session_id)
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
