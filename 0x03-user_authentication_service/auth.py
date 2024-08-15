3046999dc6cf2524a4f601ab9f67315351ded7ac
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-15 22:54:35 +0300

    Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -58,3 +58,16 @@ class Auth:
                 user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        Creates a session ID for the user with the given email.
+        Returns the session ID as a string.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
